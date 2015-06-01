e5f9c8bea7a26d01eaaffb8b95b734b66ed71533
Andrew Berls <andrew.berls@gmail.com>
2015-05-31 22:34:09 -0700

    Split modules, row reading almost done

--- src/Data/BTable.hs
+++ src/Data/BTable.hs
@@ -1,72 +1,31 @@
-{-# LANGUAGE OverloadedStrings #-}
-
-module Data.BTable where
-
-import qualified Data.ByteString as B
-import qualified Data.ByteString.Char8 as BC
-import Data.Binary
-import Data.Binary.Get
-import qualified Data.Map as Map
-import Data.String.Unicode
-
--- ASCII separator character
-sep :: Word8
-sep = 31
-
-word32ToInt :: Word32 -> Int
-word32ToInt w = (fromIntegral w) :: Int
-
--- intToWord32 :: Int -> Word32
--- intToWord32 i = (fromIntegral i) :: Word32
+--------------------------------------------------------------------
+-- |
+-- Module    : Data.BTable
+-- Copyright : (c) Andrew Berls, 2015
+-- License   : MIT
 --
--- materialize :: Int -> [(Word32, Word64)] -> [Double]
--- materialize fc indices =
---     let indexMap = Map.fromList indices
---         lookupIdx k = case Map.lookup (intToWord32 k) indexMap of
---             Just v  -> (fromIntegral v) :: Double
---             Nothing -> 0.0 :: Double
---     in map lookupIdx [0..fc]
+-- Maintainer:  andrew.berls@gmail.com
+-- Stability :  experimental
+-- Portability: portable
 --
--- -- TODO: can we get a `Get [32, 64]` signature here?
--- readIndices :: Int -> [Get (Word32, Word64)]
--- readIndices fc = map readPair [0..fc]
---     where readPair _ = do
---               idx <- getWord32be
---               v <- getWord64be
---               return (idx, v)
+-- Binary serialization format for sparse, labeled 2D numeric datasets
 --
--- getRow :: Int -> Get [Double] -- TODO: might be incorrect
--- getRow fc = do
---   indices <- readIndices fc -- TODO: have `[Get (32, 64)]`, need `Get [32, 64]`
---   return $ materialize fc indices
---
--- getRows :: Int -> Get [[Double]]
--- getRows fc = do
---     empty <- isEmpty
---     if empty
---         then return [[]]
---         else do
---           row <- getRow fc
---           rows <- getRows fc
---           return (row:rows)
+--------------------------------------------------------------------
 
-getRows fc = undefined
+module Data.BTable (
+  readVersion,
+  readLabels,
+  readRows
+  ) where
 
-readVersion :: Get Word32
-readVersion = do
-    version <- getWord32be
-    return version
+import           Data.Binary.Get
+import qualified Data.BTable.Get as BGet
+
+readVersion :: Get Int
+readVersion = BGet.readVersion
 
 readLabels :: Get [String]
-readLabels = do
-    version <- readVersion
-    labelCount <- getWord32be
-    rawLabels <- getByteString $ 2 * (word32ToInt labelCount)
-    return $ map (utf16beToUnicode . BC.unpack) (B.split sep rawLabels)
+readLabels = BGet.readLabels
 
 readRows :: Get [[Double]]
-readRows = do
-    version <- readVersion
-    labels <- readLabels
-    let featureCount = length labels
-    return $ getRows featureCount
+readRows = BGet.readRows
---
+++ src/Data/BTable/Get.hs
@@ -0,0 +1,70 @@
+module Data.BTable.Get (
+  readVersion,
+  readLabels,
+  readRows,
+  ) where
+
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as BC
+import           Data.Word
+import qualified Data.Map as Map
+import           Data.Binary.Get
+import           Data.Binary.IEEE754
+import           Data.String.Unicode
+
+-- ASCII separator character
+sep :: Word8
+sep = 31
+
+readInt :: Get Int
+readInt = fromIntegral <$> getWord32be
+
+materialize :: Int -> [(Word32, Double)] -> [Double]
+materialize fc indices =
+  let indexMap = Map.fromList indices
+      lookupIdx k = maybe 0.0 id $ Map.lookup (fromIntegral k) indexMap
+  in map lookupIdx [0..(fc - 1)]
+
+readIndices :: Int -> [Get (Word32, Double)]
+readIndices nvals = map readPair [0..(nvals - 1)]
+  where readPair _ = do
+          idx <- getWord32be
+          v <- getFloat64be
+          return (idx, v)
+
+getRow :: Int -> Get [Double]
+getRow fc = do
+  nvals <- readInt
+  indices <- sequence $ readIndices nvals
+  return $ materialize fc indices
+
+getRows :: Int -> Get [[Double]]
+getRows fc = do
+  empty <- isEmpty
+  if empty
+     then return [[]] -- TODO
+     else do
+       row <- getRow fc
+       rows <- getRows fc
+       return (row:rows)
+
+readVersion :: Get Int
+readVersion = readInt
+
+readLabels' :: Get [String]
+readLabels' = do
+  labelCount <- readInt
+  rawLabels <- getByteString $ 2 * labelCount
+  return $ map (utf16beToUnicode . BC.unpack) (B.split sep rawLabels)
+
+readLabels :: Get [String]
+readLabels = do
+  _ <- readVersion
+  readLabels'
+
+readRows :: Get [[Double]]
+readRows = do
+  _ <- readVersion
+  labels <- readLabels'
+  let featureCount = length labels
+  getRows featureCount
