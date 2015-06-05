a0ee7f13a390b381a00a191d85355c938774c982
Andrew Berls <andrew.berls@gmail.com>
2015-06-05 16:09:55 -0700

    Add get* top-level functions in terms of FilePaths

--- src/Data/BTable/Get.hs
+++ src/Data/BTable/Get.hs
@@ -1,14 +1,13 @@
--- TODO: rename to getVersion etc?
--- TODO: restructure top level in terms of filepaths
 module Data.BTable.Get (
-  readVersion,
-  readLabels,
-  readRows,
+  getVersion,
+  getLabels,
+  getRows,
   ) where
 
 import           Data.Binary.Get
 import           Data.Binary.IEEE754
 import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString.Char8 as BC
 import qualified Data.Map as Map
 import           Data.String.Unicode
@@ -20,35 +19,6 @@ sep = 31
 
 readInt :: Get Int
 readInt = fromIntegral <$> getWord32be
-
-materialize :: Int -> [(Word32, Double)] -> [Double]
-materialize fc indices =
-  let indexMap = Map.fromList indices
-      lookupIdx k = maybe 0.0 id $ Map.lookup (fromIntegral k) indexMap
-  in map lookupIdx [0..(fc - 1)]
-
-readIndices :: Int -> [Get (Word32, Double)]
-readIndices nvals = map readPair [0..(nvals - 1)]
-  where readPair _ = do
-          idx <- getWord32be
-          v <- getFloat64be
-          return (idx, v)
-
-getRow :: Int -> Get [Double]
-getRow fc = do
-  nvals <- readInt
-  indices <- sequence $ readIndices nvals
-  return $ materialize fc indices
-
-getRows :: Int -> Get [[Double]]
-getRows fc = do
-  row <- getRow fc
-  empty <- isEmpty
-  if empty
-     then return [row]
-     else do
-       rows <- getRows fc
-       return (row:rows)
 
 readVersion :: Get Int
 readVersion = readInt
@@ -64,9 +34,51 @@ readLabels = do
   readVersion
   readLabels'
 
+materialize :: Int -> [(Word32, Double)] -> [Double]
+materialize fc indices =
+  let indexMap = Map.fromList indices
+      lookupIdx k = maybe 0.0 id $ Map.lookup (fromIntegral k) indexMap
+  in map lookupIdx [0..(fc - 1)]
+
+readIndices :: Int -> [Get (Word32, Double)]
+readIndices nvals = map readPair [0..(nvals - 1)]
+  where readPair _ = do
+          idx <- getWord32be
+          v <- getFloat64be
+          return (idx, v)
+
+readRow :: Int -> Get [Double]
+readRow fc = do
+  nvals <- readInt
+  indices <- sequence $ readIndices nvals
+  return $ materialize fc indices
+
 readRows :: Get [[Double]]
 readRows = do
   readVersion
   labels <- readLabels'
   let featureCount = length labels
-  getRows featureCount
+  readRows' featureCount
+  where readRows' fc = do
+          row <- readRow fc
+          empty <- isEmpty
+          if empty
+             then return [row]
+             else do
+               rows <- readRows' fc
+               return (row:rows)
+
+getVersion :: FilePath -> IO Int
+getVersion path = do
+  contents <- BL.readFile path
+  return $ runGet readVersion contents
+
+getLabels :: FilePath -> IO [String]
+getLabels path = do
+  contents <- BL.readFile path
+  return $ runGet readLabels contents
+
+getRows :: FilePath -> IO [[Double]]
+getRows path = do
+  contents <- BL.readFile path
+  return $ runGet readRows contents
